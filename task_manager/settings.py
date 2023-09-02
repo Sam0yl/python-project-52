388da51d4ac54a835fd662a5f32648971012efcb
Evgeny Samoylov <samoyl@gmail.com>
2023-09-02 15:35:56 +0200

    base settings for project

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver', 'python-project-52-production-9c23.up.railway.app']
 
 
 # Application definition
